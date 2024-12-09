787c98baada5227ea7fe5a36330d3f35a7653e06
Aline Jéssica <167438154+alinejess@users.noreply.github.com>
2024-12-09 20:39:50 -0300

    Update comunicaserial.py

--- comunicaserial.py
+++ comunicaserial.py
@@ -11,9 +11,11 @@ baud_rate = 9600  # Certifique-se de que coincide com o configurado no Arduino
 try:
     arduino = serial.Serial(arduino_porta, baud_rate, timeout=1)
     time.sleep(1)  # Aguarda o Arduino iniciar a comunicação serial
+    # Quando o arduino está conectado
     print(f"Conectado ao Arduino na porta {arduino_porta}. Pronto para comunicação!")
 
 except serial.SerialException as e:
+    # exibido quando há algum erro inesperado
     print(f"Erro na comunicação serial: {e}")
     exit(1)
 
@@ -21,6 +23,7 @@ except serial.SerialException as e:
 def ler_serial():
     while True:
         if arduino.in_waiting > 0:
+            # decodifica a tecla recebida pelo teclado
             recebido = arduino.readline().decode('utf-8').strip()
             if recebido:
                 print(f"[Arduino] {recebido}")
@@ -34,7 +37,7 @@ try:
     print("Pressione números no teclado para enviá-los ao Arduino. Pressione 'Esc' para sair.")
     while True:
         evento = keyboard.read_event(suppress=True)
-
+        # se a tecla apertada for esc ou * o programa para
         if evento.event_type == 'down' and evento.name in ['esc', '*']:
             print("Encerrando...")
             break
@@ -48,9 +51,6 @@ try:
             print(f"Enviando número: {numero}")
             arduino.write(numero.encode())
         
-
-        
-
 except Exception as e:
     print(f"Erro: {e}")
 
