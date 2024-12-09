bf3892d245aaca63bab0d9d3630d9b80083f7dac
Aline Jéssica <167438154+alinejess@users.noreply.github.com>
2024-12-09 20:51:15 -0300

    Update teste_integracao.ino

--- teste_integracao.ino
+++ teste_integracao.ino
@@ -6,15 +6,8 @@
 #include <MFRC522.h>                        // biblioteca do sensor também
 #include <Wire.h>                           // biblioteca lcd
 #include <hd44780.h>                        // main hd44780 header
-#include <hd44780ioClass/hd44780_I2Cexp.h>  // i2c expander i/o class header
-/*
-#include <Keypad.h>                         // Biblioteca do teclado -> não vai mais precisar
-#include <Password.h>                       // Biblioteca utilizada para controle de senha -> não precisa
-#include <string.h>                         // Biblioteca para copiar string -> não precisa
-*/
-
-// teclado
-#include <PS2Keyboard.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>  // i2c expander i/o class header=
+#include <PS2Keyboard.h>                    // teclado
 
 #define RST_PIN 5  // Configurable, see typical pin layout above
 #define SS_PIN 53  // Configurable, see typical pin layout above
@@ -25,6 +18,7 @@ const int IRQpin = 3;
 
 PS2Keyboard keyboard;
 
+// struct para guardar quantas tags e senhas forem necessárias, pois se funciona apara essas, funciona para mais
 struct usuario {
   String id;
   String senha;
@@ -46,8 +40,6 @@ const int LCD_ROWS = 4;
 const int PINO_LED_VERMELHO = 13;  // LED vermelho conectado ao pino 11
 const int PINO_LED_VERDE = 11;
 
-// resto da implementação
-
 // variável de lixo para não armazenar teclas enquanto não tiver lido nenhum id
 char lixo;
 
@@ -64,7 +56,7 @@ void setup() {
   usuarios[0].senha = "8765";
 
   usuarios[1].id = "13 2E 3A AA";
-  usuarios[1].senha = "6969";
+  usuarios[1].senha = "6969";  // mudando de acordo com o que for testado no ataque
 
   usuarios[2].id = "03 A4 69 9A";
   usuarios[2].senha = "7632";
@@ -76,12 +68,13 @@ void setup() {
   lcd.begin(LCD_COLS, LCD_ROWS);
   home_lcd();  // chamando a função home para o LCD
 
+  // leitura de RFID
   while (!Serial)
-    ;                  // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-  SPI.begin();         // Init SPI bus
+    ;                  
+  SPI.begin();         
   mfrc522.PCD_Init();  // Inicia o RFID - MFRC522
-  // delay(4);                           // Optional delay. Some board do need more time after init to be ready, see Readme
-  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  // delay(4);                           
+  mfrc522.PCD_DumpVersionToSerial();  
   Serial.println(F("Saida setup"));
 
   // inicializando os LEDS
@@ -98,6 +91,7 @@ void loop() {
   // variável auxiliar que lê as teclas
   char c = keyboard.read();
 
+  // teste para verificação de cores, sem importância para o resto do código
   if (Serial.available()) {
     char c = Serial.read();
     Serial.println(c);
@@ -123,34 +117,26 @@ void loop() {
           lcd.print("   SENHA CORRETA.");
           lcd.setCursor(0, 3);
           lcd.print("  ACESSO LIBERADO.");
-          // acende o led verde caso entrada liberada
-          //analogWrite(PINO_LED_VERDE, 170);
+          // acende o led verde caso entrada liberada e deixa aceso até que o proximo id seja lido
           digitalWrite(PINO_LED_VERDE, LOW);
-          //delay(2000);
-          //digitalWrite(PINO_LED_VERDE, HIGH);
           id_encontrado = false;
-          // id = "";
           home_lcd();
           return 0;
         } else {  // Caso a senha esteja incorreta
           Serial.println();
-          Serial.println("Senha incorreta!");  // Exibe a mensagem que a senha esta correta]
+          Serial.println("Senha incorreta!");  // Exibe a mensagem que a senha esta correta
           lcd.setCursor(0, 2);
           lcd.print("  SENHA INCORRETA.");
           lcd.setCursor(0, 3);
           lcd.print("   ACESSO NEGADO.");
-          // acende o led vermelho caso entrada negada
-          //analogWrite(PINO_LED_VERMELHO, 80);
+          // acende o led vermelho caso entrada negada e deixa aceso até que o proximo id seja lido
           digitalWrite(PINO_LED_VERMELHO, LOW);
-          //delay(2000);
-          //digitalWrite(PINO_LED_VERMELHO, HIGH);
           id_encontrado = false;
-          // id = "";
           home_lcd();
           return 1;
         }
       } else {
-        lcd.print("*");
+        lcd.print("*");                     // exibindo no lcd para cada dígito 
         Serial.println(c);                  // Exibe a tecla pressionada
         senha_aux = senha_aux + String(c);  // Salva o valor da tecla pressionada na variavel senha
         Serial.println(senha_aux);
@@ -178,7 +164,7 @@ void loop() {
           lcd.clear();
           lcd.print("  DIGITE A SENHA:");
           lcd.setCursor(0, 1);
-
+          // apaga o led que esteja lidago (caso já tenha sido lido anteriormente)
           digitalWrite(PINO_LED_VERMELHO, HIGH);
           digitalWrite(PINO_LED_VERDE, HIGH);
           // leitura de senha
@@ -207,6 +193,7 @@ void loop() {
   }
 }
 
+// leitura de RFID as partir do código de IOT
 String lerRFID() {
   String id = "";
   for (byte i = 0; i < mfrc522.uid.size; i++) {
